0e107f8b26d2f7f4dc5f58d853ea71f568af85ce
Nate Diamond <nate.e.diamond@gmail.com>
2020-10-07 22:15:27 -0700

    Updates

---
+++ precache-manifest.443e4bdfa440c02adb573833a2b308ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51a5cd94b11c9928245c3cca69e0dad3",
+    "url": "/word-cuber/index.html"
+  },
+  {
+    "revision": "18bf42e386756456a8c1",
+    "url": "/word-cuber/static/css/main.223c1acf.chunk.css"
+  },
+  {
+    "revision": "fa22b832c21052e38a8b",
+    "url": "/word-cuber/static/js/2.31328791.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/word-cuber/static/js/2.31328791.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18bf42e386756456a8c1",
+    "url": "/word-cuber/static/js/main.82c3d74a.chunk.js"
+  },
+  {
+    "revision": "59584aa4c8f91505f71c",
+    "url": "/word-cuber/static/js/runtime-main.c97fb078.js"
+  }
+]);
